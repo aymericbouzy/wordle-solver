73d42b73a6d92608887de745194d15a445b16d0a
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-03-06 16:11:57 +0100

    style: tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "prettier",
-  ],
-  settings: {
-    "import/resolver": {
-      typescript: {},
-    },
-  },
-  overrides: [
-    {
-      files: ["**/*.spec.ts"],
-      env: { jest: true },
-    },
-    {
-      files: ["*.js"],
-      env: { node: true },
-    },
-  ],
+	root: true,
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"prettier",
+	],
+	settings: {
+		"import/resolver": {
+			typescript: {},
+		},
+	},
+	overrides: [
+		{
+			files: ["**/*.spec.ts"],
+			env: { jest: true },
+		},
+		{
+			files: ["*.js"],
+			env: { node: true },
+		},
+	],
 };
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
 import { add } from ".";
 
 describe("add", () => {
-  it("adds two numbers", () => {
-    expect(add(1, 2)).toBe(3);
-  });
+	it("adds two numbers", () => {
+		expect(add(1, 2)).toBe(3);
+	});
 });
