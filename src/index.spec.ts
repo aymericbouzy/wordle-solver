fc21f7291a971223d9e09db631c7fd416d2601be
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-07-24 15:27:38 +0200

    feat: compute guess

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,24 @@
-import { add } from ".";
+import { Game } from ".";
 
-describe("add", () => {
-	it("adds two numbers", () => {
-		expect(add(1, 2)).toBe(3);
+describe("guess", () => {
+	it("returns the correct pattern", () => {
+		const game = new Game("PLACE");
+
+		expect(game.guess("FORET")).toEqual([0, 0, 0, 1, 0]);
+		expect(game.guess("PLACE")).toEqual([2, 2, 2, 2, 2]);
+	});
+
+	it("handles duplicate letters in chosen word", () => {
+		const game = new Game("FETES");
+
+		expect(game.guess("FORET")).toEqual([2, 0, 0, 2, 1]);
+		expect(game.guess("BETES")).toEqual([0, 2, 2, 2, 2]);
+		expect(game.guess("CREEE")).toEqual([0, 0, 1, 2, 0]); // or [0, 0, 0, 2, 1]
+	});
+
+	it("handles duplicate letters in guess word", () => {
+		const game = new Game("TRUIE");
+
+		expect(game.guess("DATEE")).toEqual([0, 0, 1, 0, 2]);
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-export function add(a: number, b: number) {
-	return a + b;
+import assert from "assert";
+
+// // FIXME
+// type Char = string;
+// FIXME
+type Word = string;
+// type SplitWord = [Char, Char, Char, Char, Char];
+type PatternChar = 0 | 1 | 2;
+type Pattern = [
+	PatternChar,
+	PatternChar,
+	PatternChar,
+	PatternChar,
+	PatternChar
+];
+
+const fiveLetterWord = /^[A-Z]{5}$/;
+
+export class Game {
+	constructor(private readonly chosenWord: Word) {
+		assert(fiveLetterWord.test(this.chosenWord));
+	}
+
+	guess(guessWord: Word): Pattern {
+		assert(fiveLetterWord.test(guessWord));
+
+		const letterCounts = this.chosenWord.split("").reduce((counts, char) => {
+			if (!counts[char]) counts[char] = 0;
+
+			counts[char] += 1;
+
+			return counts;
+		}, {} as Record<string, number>);
+
+		const pattern = Array(5).fill(0) as Pattern;
+
+		guessWord.split("").forEach((char, index) => {
+			if (this.chosenWord[index] === char) {
+				letterCounts[char] -= 1;
+
+				pattern[index] = 2;
+			}
+		});
+
+		guessWord.split("").forEach((char, index) => {
+			if (this.chosenWord[index] !== char && letterCounts[char]) {
+				letterCounts[char] -= 1;
+
+				pattern[index] = 1;
+			}
+		});
+
+		return pattern;
+	}
 }
