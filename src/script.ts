da1be3dbba29aa0d0e9f389d07546d5a9faeed13
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-08-16 15:39:55 +0200

    feat: display steps

--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,30 @@
+import chalk from "chalk";
 import { getRemainingWords, Pattern, solve, Word } from ".";
 import words from "./words.json";
+
+const steps: [Word, Pattern][] = [["AIMER", "10020"]];
+
+for (const [word, pattern] of steps) {
+	console.log(
+		word
+			.split("")
+			.map((char, index) => {
+				if (pattern[index] === "2") {
+					return chalk.bgGreen(char);
+				} else if (pattern[index] === "1") {
+					return chalk.bgYellow(char);
+				} else {
+					return chalk.bgGray(char);
+				}
+			})
+			.join("")
+	);
+}
 
 console.log(
 	solve({
 		possibleWords: words,
-		remainingWords: ([["AIMER", "00001"]] as [Word, Pattern][]).reduce(
+		remainingWords: steps.reduce(
 			(words, [guess, pattern]) => getRemainingWords(guess, pattern, words),
 			words
 		),
